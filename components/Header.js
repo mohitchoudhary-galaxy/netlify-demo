f043c0ea3c76a0d188b19d28cf58a6b77c700e46
mohitgalaxy <mohit.choudhary@galaxyweblinks.in>
2023-01-25 19:53:57 +0530

    test

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
-import Clogo from '../assets/galaxy_logo.png';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4">
-        <img src={Clogo} alt="logo" />
+        <Image
+          src="../assets/galaxy_logo.png"
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
       </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
