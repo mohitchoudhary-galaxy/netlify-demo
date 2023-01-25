8fb81308287a1b2df203c4a1bc99a868a6d355ce
mohitgalaxy <mohit.choudhary@galaxyweblinks.in>
2023-01-25 19:58:53 +0530

    removed default logo

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4">
+      <div className="w-12 h-12">
         <Image
           src="../assets/galaxy_logo.png"
           alt="Picture of the author"
